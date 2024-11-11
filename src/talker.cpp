86ceebc193bfffa7ba6a1821956e2049efcc30d0
piyush-g0enka <goenkapiyush@uahoo.co.in>
2024-11-10 23:50:54 -0500

    created ros publisher and subscriber

---
+++ src/talker.cpp
@@ -0,0 +1,41 @@
+// Copyright 2024 Piyush Goenka
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node {
+ public:
+  MinimalPublisher() : Node("minimal_publisher"){
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+  }
+
+ private:
+  void timer_callback() {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello from ros2";
+    RCLCPP_INFO(this->get_logger(), "Hello from ROS2");
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  //size_t count_;
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
